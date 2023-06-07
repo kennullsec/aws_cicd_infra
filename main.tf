302d35415ac8d316dfe48a4539b41b16df28abdb
ken <kennullsec@gmail.com>
2023-06-07 22:51:53 +0530

    v1

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {}
+
+resource "aws_instance" "web" {
+
+    ami = "ami-a0cfeed8"
+    instance_type =  "t2.micro"
+    vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+    tags = {
+        Name = random_pet.name.id
+    }
+  
+}
+
+resource "aws_security_group" "web-sg" {
+
+  name        = "${random_pet.name.id}-sg"
+  description = "allow ssh and http traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
