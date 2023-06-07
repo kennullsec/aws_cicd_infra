fbd2a6be538f23e45da0d9b4e4266e21f89566f4
ken <kennullsec@gmail.com>
2023-06-07 22:57:01 +0530

    v1

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_pet" "name" {}
 
 resource "aws_instance" "web" {
 
-    ami = "ami-a0cfeed8"
+    ami = "ami-05fa00d4c63e32376"
     instance_type =  "t2.micro"
     vpc_security_group_ids = [aws_security_group.web-sg.id]
 
