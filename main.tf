f1c94ee9f93f02b72d26968aef1059b6ee4879e0
ken <kennullsec@gmail.com>
2023-06-08 15:55:33 +0530

    vpc

--- main.tf
+++ main.tf
@@ -18,6 +18,76 @@ resource "aws_instance" "web" {
   
 }
 
+resource "aws_vpc" "main" {
+
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      Name = "${random_pet.name.id}-vpc"
+    }
+
+}
+
+resource "aws_subnet" "public_subnets" {
+
+    count      = length(var.public_subnet_cidrs)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = element(var.public_subnet_cidrs, count.index)
+    availability_zone = element(var.azs, count.index)
+
+    tags = {
+      Name = "${random_pet.name.id}-Public-Subnet-${count.index + 1}"
+    }
+
+}
+
+resource "aws_subnet" "private_subnets" {
+
+    count      = length(var.private_subnet_cidrs)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = element(var.private_subnet_cidrs, count.index)
+    availability_zone = element(var.azs, count.index)
+
+    tags = {
+      Name = "${random_pet.name.id}-Private-Subnet-${count.index + 1}"
+    }
+
+}
+
+resource "aws_internet_gateway" "gw" {
+
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+      Name = "${random_pet.name.id}-Project-VPC-IG"
+    }
+
+}
+
+
+resource "aws_route_table" "second_rt" {
+
+    vpc_id = aws_vpc.main.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
+
+    tags = {
+      Name = "${random_pet.name.id}-2nd-Route-Table"
+    }
+
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+
+    count = length(var.public_subnet_cidrs)
+    subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+    route_table_id = aws_route_table.second_rt.id
+
+}
+
+
 resource "aws_security_group" "web-sg" {
 
   name        = "${random_pet.name.id}-sg"
